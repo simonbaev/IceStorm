af56e18b358687ee6c15e6c1b5033cf22e866dce
Simon Baev <simonbaev@gmail.com>
2016-02-02 00:35:43 -0500

    att+mid+fin

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -10,17 +10,17 @@ function termChangeHandler(ev) {
 	});
 }
 
-function addClassesByCRN(container, options, i) {
+function addClassesByCRN(container, CRNs, i) {
 	//-- Termination condition
-	if(i <= 0) {
+	if(i < -CRNs.length) {
 		return;
 	}
-	var index = options.length - i;
-	var opt = options.eq(index);
-	var desc = opt.text().split(/,/)[0];
+	var index = CRNs.length - i;
+	var CRN = CRNs.eq(index);
+	var desc = CRN.text().split(/,/)[0];
 	var courseNumbers = desc.split(/:/)[0].trim().split(/\s+/g);
 	var courseTitle = desc.split(/:/)[1].trim();
-	var crn = opt.val();
+	var crnValue = CRN.val();
 	container
 	.append(
 		$('<div>')
@@ -39,6 +39,7 @@ function addClassesByCRN(container, options, i) {
 					$('<a>')
 					.attr({
 						'role': 'button',
+						'id': 'ta_' + (index+1),
 						'data-toggle': 'collapse',
 						'href': '#tc_' + (index+1),
 						'aria-expanded': false,
@@ -47,6 +48,9 @@ function addClassesByCRN(container, options, i) {
 					.html('<strong>' + courseNumbers.slice(0,2).join(' ') + '<small>&nbsp;(' + courseNumbers[2] + ')</small></strong>:&nbsp' + courseTitle)
 				)
 			)
+			.click(function() {
+				$(this).next('div.collapse').collapse('toggle');
+			})
 		)
 		.append(
 			$('<div>')
@@ -61,15 +65,37 @@ function addClassesByCRN(container, options, i) {
 				.addClass('panel-body')
 				.append(
 					$('<table>')
-					.addClass('table table-bordered table-hover table-condensed')
+					.addClass('table table-bordered table-condensed')
 					.append(
 						$('<thead>')
 						.append(
 							$('<tr>')
-							.append($('<th>').text('#').addClass('td_#'))
+							.append($('<th>').text('#').addClass('td_N'))
 							.append($('<th>').text('Name').addClass('td_name'))
 							.append($('<th>').text('GSW ID').addClass('td_id'))
+							.append(
+								$('<th>')
+								.addClass('td_att')
+								.append(
+									$('<div>')
+									.addClass('checkbox')
+									.append(
+										$('<label><input type="checkbox"><b>Att.</b></label>')
+									)
+									.find('input')
+									.change(function(){
+										var state = this.checked;
+										$(this).parents('table.table').find('tbody tr td input').each(function(){
+											this.checked = state;
+										});
+									})									
+									.end()
+								)
+							)
+							.append($('<th>').text('Midterm').addClass('td_mid'))
+							.append($('<th>').text('Final').addClass('td_fin'))
 							.append($('<th>').text('E-Mail').addClass('td_email'))
+							
 						)
 					)	
 					.append($('<tbody>'))
@@ -77,12 +103,13 @@ function addClassesByCRN(container, options, i) {
 			)
 		)
 	);
+	//-- Set CRN and in case of success retrieve class roster
 	$.ajax({
 		type: 'POST',
 		async: true,
 		url: 'https://gsw.gabest.usg.edu/pls/B420/bwlkocrn.P_FacStoreCRN',
 		contentType: 'application/x-www-form-urlencoded',
-		data: 'name1=bmenu.P_FacMainMnu&calling_proc_name=P_FACCRNSEL&crn=' + crn,							
+		data: 'name1=bmenu.P_FacMainMnu&calling_proc_name=P_FACCRNSEL&crn=' + crnValue,							
 		error: function(e) {
 			console.log(e);
 		},
@@ -102,12 +129,67 @@ function addClassesByCRN(container, options, i) {
 						var name = tr.find('td:eq(1)').text();
 						var lname = name.split(/,/)[0].trim();
 						var fname = name.split(/,/)[1].trim().split(/\s/,1)[0].trim();
+						var wd = /WD/.test(tr.find('td:eq(3)').text());
 						$('#tc_' + (index+1) + ' table.table tbody')
 						.append(
 							$('<tr>')
+							.addClass(wd ? 'bg-danger' : '')
+							.attr({
+								'title': wd ? tr.find('td:eq(3)').text().trim() : ''
+							})
 							.append($('<td>').text(tr.find('td:eq(0)').text()))
 							.append($('<td>').text(name))
 							.append($('<td>').text(tr.find('td:eq(2)').text()))
+							.append(
+								$('<td>')
+								.append(
+									$('<div>')
+									.append(
+										$('<input>')
+										.attr({
+											'type':'checkbox'
+										})
+										.click(function(){
+											this.checked = !this.checked;
+										})
+									)
+									.click(function(){
+										$(this).find('input[type="checkbox"]').each(function(){ 
+											this.checked = !this.checked; 
+										});
+									})
+								)							
+							)						
+							.append(
+								$('<td>')
+								.append(
+									$('<select>')
+									.addClass('form-control')
+									.append($('<option>').val(null).text('None').attr('selected',''))									
+									.append($('<option>').val('A').text('A'))									
+									.append($('<option>').val('B').text('B'))									
+									.append($('<option>').val('C').text('C'))									
+									.append($('<option>').val('D').text('D'))									
+									.append($('<option>').val('F').text('F'))									
+									.append($('<option>').val('I').text('I'))									
+									.append($('<option>').val('I').text('P'))									
+								)
+							)
+							.append(
+								$('<td>')
+								.append(
+									$('<select>')
+									.addClass('form-control')
+									.append($('<option>').val(null).text('None').attr('selected',''))
+									.append($('<option>').val('A').text('A'))									
+									.append($('<option>').val('B').text('B'))									
+									.append($('<option>').val('C').text('C'))									
+									.append($('<option>').val('D').text('D'))									
+									.append($('<option>').val('F').text('F'))									
+									.append($('<option>').val('I').text('I'))									
+									.append($('<option>').val('I').text('P'))									
+								)
+							)
 							.append(
 								$('<td>')
 								.append(
@@ -122,7 +204,7 @@ function addClassesByCRN(container, options, i) {
 							)
 						);
 					});
-					addClassesByCRN(container, options, i-1);
+					addClassesByCRN(container, CRNs, i-1);					
 				}
 			});
 		}
@@ -146,7 +228,12 @@ function updateTerm(termString) {
 				success: function(data) {
 					var html = $($.parseHTML(data));
 					var CRNs = html.find('select[name=crn] option');
-					addClassesByCRN($('#accordion').empty(), CRNs, CRNs.length);
+					if(CRNs.length) {
+						addClassesByCRN($('#accordion').empty(), CRNs, CRNs.length);						
+					}
+					else {
+						$('#accordion').empty();
+					}
 				}
 			});
 		}
@@ -166,7 +253,7 @@ $(document).ready(function(){
 	chrome.storage.local.get(['termSelect','termYear','termString'],function(items){
 		$('#termSelect option[value="' + items.termSelect + '"]').prop('selected', true);
 		$('#termYear').val(items.termYear);		
-		updateTerm(items.termString);
+		updateTerm(items.termString);		
 	});
 
 	$('#termSelect').change(termChangeHandler);
