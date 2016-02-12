e7dbab75e92faee20fafa116a7d3b6372c533264
Simon Baev <simonbaev@gmail.com>
2016-02-11 22:36:29 -0500

    print workaround option removed

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,20 +1,16 @@
 function restore_options() {
-	chrome.storage.local.get(['optPrintWork','optKeepAlive'], function(items) {
-		document.getElementById('cbPrint').checked = items.optPrintWork;
+	chrome.storage.local.get(['optKeepAlive'], function(items) {
 		document.getElementById('cbKeepAlive').checked = items.optKeepAlive;		
 	});
 }
 
 function save_options() {
-	var optPrintWork = document.getElementById('cbPrint').checked;
 	var optKeepAlive = document.getElementById('cbKeepAlive').checked;
 	chrome.storage.local.set({
-		optPrintWork: optPrintWork,
 		optKeepAlive: optKeepAlive
 	});
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 
-$('#cbPrint').on('change',save_options);
 $('#cbKeepAlive').on('change',save_options);
